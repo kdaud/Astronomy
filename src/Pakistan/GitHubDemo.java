7ddb1a8a83997173d028451827d6468eda29d459
Kakumirizi Daud <dkakumirizii@gmail.com>
2019-02-04 02:06:18 -0800

    Added annotation

--- src/Pakistan/GitHubDemo.java
+++ src/Pakistan/GitHubDemo.java
@@ -3,8 +3,10 @@ package Pakistan;
 import java.util.Enumeration;
 import java.util.Stack;
 
+@SuppressWarnings("all")
 public class GitHubDemo {
 	public static void main(String[] args) {
+		//this a single line comment
 		flit();
 	}
 	public static void flit()
